319b234cfd9164aec3fd615953ed16864f49b1d0
saipriyagoka <Priyareddy2712@gmail.com>
2019-11-05 23:38:45 +0530

    make it better

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\saipriya\desktop\priya\my_django_stuff\myvideouploader\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
