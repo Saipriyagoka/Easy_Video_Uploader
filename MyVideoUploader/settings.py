63ac8e6b4f6354320d9ae2c07282ed71390b4c5d
saipriyagoka <Priyareddy2712@gmail.com>
2019-11-11 14:30:17 +0530

    make it better

--- MyVideoUploader/settings.py
+++ MyVideoUploader/settings.py
@@ -50,12 +50,12 @@ DATABASES = {
     'extra': env.db('SQLITE_URL', default='sqlite:////tmp/my-tmp-sqlite.db')
 }
 
-CACHES = {
-    # read os.environ['CACHE_URL'] and raises ImproperlyConfigured exception if not found
-    'default': env.cache(),
-    # read os.environ['REDIS_URL']
-    'redis': env.cache('REDIS_URL')
-}
+# CACHES = {
+#     # read os.environ['CACHE_URL'] and raises ImproperlyConfigured exception if not found
+#     'default': env.cache(),
+#     # read os.environ['REDIS_URL']
+#     'redis': env.cache('REDIS_URL')
+# }
 
 
 
