abc1fd5c9bc5db6bece44c3e0946358474bead4f
saipriyagoka <Priyareddy2712@gmail.com>
2019-11-11 14:34:40 +0530

    make it better

--- .env.py
+++
@@ -1,7 +0,0 @@
-
-DEBUG=on
-SECRET_KEY='8ru#9fu36z+!l+#mw84emkpna2zms&3t2qerrt()=b(6_a9clt'
-DATABASE_URL='mysql://b30f85f23c2a32:cddcfafd@us-cdbr-iron-east-05.cleardb.net/heroku_1fc8990caab8b08'
-CLEARDB_DATABASE_URL='mysql://b30f85f23c2a32:cddcfafd@us-cdbr-iron-east-05.cleardb.net/heroku_1fc8990caab8b08?reconnect=true'
-DISABLE_COLLECTSTATIC=1
-WEB_CONCURRENCY=3
